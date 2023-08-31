0b99bd8428a9b70a2f25220dca4b41e8bcb1a021
Hiago Emanuel <hiagoemanuel321@hotmail.com>
2023-08-31 17:45:36 -0300

    vite construido

--- src/constants/contact/contact-images.js
+++
@@ -1,9 +0,0 @@
-import github from '../../assets/icons/contact-icons/github-contact-icon.svg'
-import linkedin from '../../assets/icons/contact-icons/linkedin-icon.svg'
-import whatsapp from '../../assets/icons/contact-icons/whatsapp-icon.svg'
-
-export const contacImages = {
-    github,
-    linkedin,
-    whatsapp
-}
