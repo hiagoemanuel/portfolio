5ab9e0514d7631e0fba6ae167f2e8017b435585b
hiagoemanuel <hiagoemanuel321@hotmail.com>
2024-05-22 11:01:45 -0300

    style(Technologies): responsive

--- src/components/TechCard.tsx
+++ src/components/TechCard.tsx
@@ -1,8 +1,10 @@
 export const TechCard = ({ name, icon }: { name: string; icon: JSX.Element }) => {
   return (
-    <div className="w-37 h-18 rounded-lg flex flex-col justify-center items-center gap-1 bg-main">
-      <div className="h-10">{icon}</div>
-      <h5 className="text-xs font-bold text-light dark:text-dark">{name}</h5>
+    <div className="w-37 xs:w-48 sm:w-76 h-18 xs:h-24 sm:h-36 rounded-lg sm:rounded-xl flex flex-col justify-center items-center gap-1 bg-main">
+      <div className="h-10 xs:h-14 sm:h-20">{icon}</div>
+      <h5 className="text-xs xs:text-base sm:text-base font-bold text-light dark:text-dark">
+        {name}
+      </h5>
     </div>
   )
 }
--- src/components/sections/Technologies.tsx
+++ src/components/sections/Technologies.tsx
@@ -9,10 +9,10 @@ export const Technologies = () => {
   return (
     <Container>
       <Title title="tecnologias" japTitle="テクノロジー" />
-      <p className="font-normal text-xs text-center mb-3 text-dark dark:text-light">
+      <p className="font-normal text-xs xs:text-sm sm:text-2xl text-center xl:text-start mb-3 text-dark dark:text-light">
         Essas são as principais tecnologias que costumo utilizar.
       </p>
-      <div className="grid grid-cols-[repeat(2,_9.25rem)] grid-rows-3 gap-2 justify-center">
+      <div className="grid grid-cols-[repeat(2,max-content)] lg:grid-cols-[repeat(3,max-content)] grid-rows-3 lg:grid-rows-2 gap-2 lg:gap-5 justify-center">
         <TechCard name="Next.js" icon={<NextJs />} />
         <TechCard name="Next.js" icon={<NextJs />} />
         <TechCard name="Next.js" icon={<NextJs />} />
