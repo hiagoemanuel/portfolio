a4600c4260a192626586dcb408ba4706f486379d
hiagoemanuel <hiagoemanuel321@hotmail.com>
2024-05-23 15:29:11 -0300

    style(Contact): desktop responsive

--- src/components/ContactNetworks.tsx
+++ src/components/ContactNetworks.tsx
@@ -13,9 +13,11 @@ export const ContactNetworks = () => {
   }, [])
 
   return (
-    <div className="flex flex-col gap-3 sm:gap-6 items-center">
+    <div className="flex flex-col lg:items-start lg:self-start gap-3 sm:gap-6 items-center">
       <div>
-        <h4 className="mb-2 sm:mb-4 text-xl sm:text-4xl font-bold text-center">minhas redes</h4>
+        <h4 className="mb-2 sm:mb-4 text-xl sm:text-4xl font-bold text-center lg:text-start">
+          minhas redes
+        </h4>
         <div className="flex flex-col gap-1 sm:gap-3">
           <Link className="flex items-end gap-1" href="/">
             <div className="w-10 sm:w-18 h-10 sm:h-18 p-2 sm:p-3 rounded-lg sm:rounded-xl bg-main">
@@ -32,10 +34,10 @@ export const ContactNetworks = () => {
         </div>
       </div>
       <div>
-        <h4 className="mb-2 sm:mb-4 text-xl sm:text-4xl font-bold text-center">
+        <h4 className="mb-2 sm:mb-4 text-xl sm:text-4xl font-bold text-center lg:text-start">
           e-mail de contato
         </h4>
-        <p className="text-sm sm:text-3xl font-medium text-center">{email}</p>
+        <p className="text-sm sm:text-3xl font-medium text-center lg:text-start">{email}</p>
       </div>
     </div>
   )
--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -7,7 +7,7 @@ export const Contact = () => {
   return (
     <Container>
       <Title title="contato" japTitle="接触" />
-      <div className="flex flex-col items-center gap-4 sm:gap-10">
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-4 sm:gap-10">
         <form
           className="w-full max-w-96 flex flex-col gap-2 sm:gap-3 px-2"
           action="/"
@@ -17,7 +17,7 @@ export const Contact = () => {
           <input className="input-form" placeholder="nome *" type="text" name="name" />
           <input className="input-form" placeholder="email *" type="email" name="email" />
           <input className="input-form" placeholder="assunto" type="text" name="subject" />
-          <textarea className="input-form h-26 " placeholder="mensagem *" name="message" />
+          <textarea className="input-form h-26 lg:h-48" placeholder="mensagem *" name="message" />
           <input
             className="input-form cursor-pointer text-xl font-bold"
             type="submit"
