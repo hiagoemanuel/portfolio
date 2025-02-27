319fc5a119267aa533c9b7da35ca55cb0a0f9108
hiagoemanuel <hiagoemanuel321@hotmail.com>
2025-02-26 21:55:00 -0300

    fix(style): handle text color theme from error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -5,7 +5,6 @@ import { useState } from 'react'
 export default function Error({ error }: { error: Error & { digest?: string } }) {
   const [isOpen, setIsOpen] = useState<boolean>(false)
 
-  console.log(error)
   return (
     <main className="full-viewport flex flex-col justify-center items-center">
       <h1 className="text-9xl font-extrabold text-main">500</h1>
@@ -23,7 +22,7 @@ export default function Error({ error }: { error: Error & { digest?: string } })
         className={`${isOpen ? 'flex' : 'hidden'} justify-center items-center absolute top-0 left-0 w-full h-svh bg-light/50 dark:bg-dark/50`}
       >
         <div className="max-w-[37.5rem] bg-main rounded-xl p-3 mx-3 flex flex-col items-center">
-          <p className="bg-dark rounded-xl p-3">{error.message}</p>
+          <p className="bg-dark rounded-xl p-3 text-light">{error.message}</p>
           <button
             className="text-xl mt-3 p-3 rounded-xl font-medium bg-light dark:bg-dark"
             onClick={() => {
